9b163939c0c9b55007a9803eca8abda3fe40bcb0
mahidulmoon <mahidulmoon@gmail.com>
2021-02-10 12:10:35 +0600

    removing html form handle to js onSubmit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,31 @@
 import { rootCertificates } from "tls"
-
+import {useState} from "react"
+import jwt from 'jsonwebtoken'
 
 function Home(props) {
+  const [username,setUsername] = useState<string>('')
+  const [password,setPassword] = useState<string>('')
   const variable = Math.random() >.5 ? 'red' : 'blue'
+  const [message,setMessage] = useState<string>('')
+  async function submitForm(){
+    const res = await fetch('api/login',{
+      method:'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify({username,password})
+    }).then((t) => t.json())
+    const token = res.token
+    if(token){
+      const json = jwt.decode(token)
+      setMessage(`welcom ${json.username} and you are ${json.admin ? 'an admin' : 'not an admin'}`)
+    }else{
+      setMessage("something went wrong")
+    }
+  }
   return(
     <div>
+      <h1>{message}</h1>
       <h1 className="title">{props.text}</h1>
       <style jsx>
           {`
@@ -15,12 +36,12 @@ function Home(props) {
       </style>
       <h2 className="custom">This is to test from golbal css file</h2>
 
-      <form method="POST" action="api/login">
-        <input type="text" name="username" value="admin" />
+      <form>
+        <input type="text" name="username" value={username} onChange={e => setUsername(e.target.value)} />
         <br />
-        <input type="password" name="password" value="admin" />
+        <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)}/>
         <br />
-        <input type="submit" value="login" />
+        <input type="button" value="login" onClick={submitForm} />
       </form>
     </div>
   )
