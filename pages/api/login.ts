87e6a48da7065323c0da7727ad0da793b98852e7
mahidulmoon <mahidulmoon@gmail.com>
2021-02-10 11:47:18 +0600

    implementing jwt authentication

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,7 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
+import jwt from 'jsonwebtoken'
+const KEY = 'aslkfjasdfoaisdfalsdfal;sdfoasdifjf'
 
 export default function (req:NextApiRequest,res:NextApiResponse){
-    console.log(req.body)
-    res.json({message:"success"})
+    if(!req.body){
+        req.statusCode = 404
+        res.end('Error')
+        return
+    }
+    const {username,password} = req.body
+    //console.log(req.body)
+    //res.json({message:"success"})
+    res.json({
+        token: jwt.sign(
+            {
+                username,
+                admin: username==='admin' && password ==='admin'
+            },
+            KEY
+        )
+    })
 }
