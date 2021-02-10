a6e1b1a48af4981da6319ab0a45654accb54e695
mahidulmoon <mahidulmoon@gmail.com>
2021-02-10 17:38:25 +0600

    dynamic routing added

---
+++ pages/filename/[name]/[subfile].tsx
@@ -0,0 +1,6 @@
+export default function FilaName(){
+    return <h1>Dynamic routing is working</h1>
+}
+
+//http://localhost:3000/filename/abc/123 -> working 
+//http://localhost:3000/filename/abc/ -> 404
